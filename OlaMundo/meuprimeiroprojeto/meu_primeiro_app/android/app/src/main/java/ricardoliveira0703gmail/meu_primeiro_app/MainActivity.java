21070386644317ed471a65f5956fda0a86720c7e
ricardoliveiraof2m <ricardoliveira0703@gmail.com>
2021-10-15 14:35:12 -0300

    Ola Mundo !

--- OlaMundo/meuprimeiroprojeto/meu_primeiro_app/android/app/src/main/java/ricardoliveira0703gmail/meu_primeiro_app/MainActivity.java
+++
@@ -1,6 +0,0 @@
-package ricardoliveira0703gmail.meu_primeiro_app;
-
-import io.flutter.embedding.android.FlutterActivity;
-
-public class MainActivity extends FlutterActivity {
-}
--- OlaMundo/meuprimeiroprojeto/meu_primeiro_app/lib/main.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-void main (){
-  runApp(new MaterialApp(
-    title: "Frases do dia",
-    home: Container(color: Colors.yellow,),
-  ));
-}
